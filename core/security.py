400fd2df1a4fd1d77901ea40cd70453706a1b3b6
Lincoln Taranto <54151306+lincolntaranto@users.noreply.github.com>
2026-06-10 12:12:36 -0300

    feat: cria a funcao 'create_access_token

--- core/security.py
+++ core/security.py
@@ -1,3 +1,8 @@
+from datetime import timedelta, datetime, timezone
+from typing import Any
+
+from core.config import ACCESS_TOKEN_EXPIRE_MINUTES, SECRET_KEY
+
 import jwt
 
 from pwdlib import PasswordHash
@@ -9,3 +14,14 @@ def verify_password(plain_password: str, hashed_password: str):
 
 def get_password_hash(password: str):
     return password_hash.hash(password)
+
+ALGORITHM = "HS256"
+
+def create_access_token(subject: str | Any, expires_delta: timedelta = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)):
+    expire = datetime.now(timezone.utc) + expires_delta
+    to_encode = {
+        "sub": str(subject),
+        "exp": expire
+    }
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
