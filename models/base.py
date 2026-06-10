e91dc977ca3a5ff969e1f3fed3a8f8afc204fc4f
Lincoln Taranto <54151306+lincolntaranto@users.noreply.github.com>
2026-06-10 01:34:06 -0300

    feat: cria a classe Base

---
+++ models/base.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+db_url = os.getenv("DATABASE_URL")
+
+if not db_url:
+    raise ValueError("DATABASE_URL não encontrada.")
+
+db = create_engine(db_url)
+
+class Base(DeclarativeBase):
+    pass
