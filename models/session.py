dfc8add213edbbf4cfc69959edc698c5c64ed556
Lincoln Taranto <54151306+lincolntaranto@users.noreply.github.com>
2026-06-10 01:50:59 -0300

    feat: cria a funcao get_session

---
+++ models/session.py
@@ -0,0 +1,8 @@
+from sqlalchemy.orm import sessionmaker, Session
+from base import db
+
+SessionLocal = sessionmaker(db)
+
+def get_session():
+    with SessionLocal() as session:
+        yield session
