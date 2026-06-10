8180d1b82d73ee11113df192804eb7252e3a4583
Lincoln Taranto <54151306+lincolntaranto@users.noreply.github.com>
2026-06-10 01:44:12 -0300

    feat: cria os schemas UserCreateSchema e o UserResponse

---
+++ schemas/user.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel, EmailStr, ConfigDict
+
+
+class UserCreateSchema(BaseModel):
+    name: str
+    password: str
+    email: EmailStr
+
+class UserResponse(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    name: str
+    email: EmailStr
+    admin: bool
