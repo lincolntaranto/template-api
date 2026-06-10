a484e8e8e2c33f22fcf2452027a5780a6eb077bc
Lincoln Taranto <54151306+lincolntaranto@users.noreply.github.com>
2026-06-10 13:06:42 -0300

    feat: atualiza para usar a classe Settings do pydantic_settings

--- core/config.py
+++ core/config.py
@@ -1,9 +1,14 @@
 from fastapi.security import OAuth2PasswordBearer
-from dotenv import load_dotenv
-import os
+from pydantic_settings import BaseSettings
 
-load_dotenv()
 
-SECRET_KEY = os.getenv("SECRET_KEY")
-ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
+class Settings(BaseSettings):
+    DATABASE_URL: str
+    SECRET_KEY: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+
+    model_config = {"env_file": ".env"}
+
+settings = Settings()
+
 oauth2_schema = OAuth2PasswordBearer(tokenUrl="auth/login-form")
