0511415967e34f4d9e92c307019c0331e1c757ac
lincolntaranto <54151306+lincolntaranto@users.noreply.github.com>
2026-06-11 00:11:44 -0300

    feat: adiciona a rota user

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 
 from routes.auth_routes import auth_router
+from routes.user_routes import user_router
 
 app = FastAPI()
 
 app.include_router(auth_router)
+app.include_router(user_router)
