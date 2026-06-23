052ac776c08120fd4e6ca74333203726d7528a2e
lincolntaranto <54151306+lincolntaranto@users.noreply.github.com>
2026-06-23 16:33:46 -0300

    feat: adiciona uma instancia do Limiter(slowAPI)

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from slowapi import Limiter
+from slowapi.util import get_remote_address
 
-from core.config import Settings, settings
+from core.config import settings
 from routes.auth_routes import auth_router
 from routes.user_routes import user_router
+
+limiter = Limiter(
+    key_func=get_remote_address,
+    strategy="fixed-window",
+    storage_options={"uri": "memory://"}
+)
+
 
 app = FastAPI(
     title=settings.PROJECT_NAME
