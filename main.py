cf6c6e8b848b939a2f8dc551b801411d81ced3e0
lincolntaranto <54151306+lincolntaranto@users.noreply.github.com>
2026-06-24 17:49:21 -0300

    fix: move o limiter para um arquivo proprio para evitar importacao circular

--- main.py
+++ main.py
@@ -1,18 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from slowapi import Limiter
-from slowapi.util import get_remote_address
 
 from core.config import settings
 from routes.auth_routes import auth_router
 from routes.user_routes import user_router
-
-limiter = Limiter(
-    key_func=get_remote_address,
-    strategy="fixed-window",
-    storage_options={"uri": "memory://"}
-)
-
 
 app = FastAPI(
     title=settings.PROJECT_NAME
