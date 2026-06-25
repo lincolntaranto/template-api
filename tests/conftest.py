9f2cd2fb50c82c909a6ff36baff117f4365a55a1
lincolntaranto <54151306+lincolntaranto@users.noreply.github.com>
2026-06-25 07:46:49 -0300

    style: formatacao com ruff

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,9 +12,11 @@ from models.session import get_session
 
 test_engine = create_engine(settings.DATABASE_TEST_URL)
 
+
 def override_get_session():
     with Session(test_engine) as session:
         yield session
+
 
 @pytest.fixture(scope="session", autouse=True)
 def criar_tabelas():
@@ -22,12 +24,14 @@ def criar_tabelas():
     yield
     Base.metadata.drop_all(bind=test_engine)
 
+
 @pytest.fixture(scope="session")
 def client():
     app.dependency_overrides[get_session] = override_get_session
     with TestClient(app) as c:
         yield c
     app.dependency_overrides.clear()
+
 
 @pytest.fixture(autouse=True)
 def disable_limiter():
