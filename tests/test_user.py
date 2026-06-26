827e652ee5c53dc99ec6a6cb7538891a1753bb8d
lincolntaranto <54151306+lincolntaranto@users.noreply.github.com>
2026-06-26 17:38:30 -0300

    test: cria o arquivo 'test_user' com o teste 'test_change_password' que testa a rota 'user/password'

---
+++ tests/test_user.py
@@ -0,0 +1,12 @@
+from tests.conftest import USER_DATA
+
+CHANGE_PASSWORD = {"current_password": USER_DATA["password"], "new_password": "1234"}
+
+
+def test_change_password(token, client):
+    response = client.patch(
+        "/user/password",
+        json=CHANGE_PASSWORD,
+        headers={"Authorization": f"Bearer {token}"},
+    )
+    assert response.status_code == 200
