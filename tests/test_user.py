565a2336f4cf5a15c74adb7875fc959b4aa12306
lincolntaranto <54151306+lincolntaranto@users.noreply.github.com>
2026-06-26 17:42:18 -0300

    test: cria o teste 'test_change_password_equal' que testa a a rota '/user/password' paro o caso do usuario tentar botar a mesma senha na troca

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,6 +1,10 @@
 from tests.conftest import USER_DATA
 
 CHANGE_PASSWORD = {"current_password": USER_DATA["password"], "new_password": "1234"}
+CHANGE_PASSWORD_EQUAL = {
+    "current_password": USER_DATA["password"],
+    "new_password": "123",
+}
 
 
 def test_change_password(token, client):
@@ -10,3 +14,12 @@ def test_change_password(token, client):
         headers={"Authorization": f"Bearer {token}"},
     )
     assert response.status_code == 200
+
+
+def test_change_password_equal(token, client):
+    response = client.patch(
+        "/user/password",
+        json=CHANGE_PASSWORD_EQUAL,
+        headers={"Authorization": f"Bearer {token}"},
+    )
+    assert response.status_code == 400
