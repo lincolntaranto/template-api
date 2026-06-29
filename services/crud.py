d6cb348b6cc854bd50e1cb8927578f5eee619aae
lincolntaranto <54151306+lincolntaranto@users.noreply.github.com>
2026-06-29 19:59:48 -0300

    feat: cria a funcao 'get_username_by_email'

--- services/crud.py
+++ services/crud.py
@@ -9,3 +9,10 @@ def get_user_by_email(*, session: Session, email: str) -> User | None:
         select(User).where(User.email == email)
     ).scalar_one_or_none()
     return session_user
+
+
+def get_username_by_email(*, session: Session, email: str) -> str | None:
+    session_user = session.execute(
+        select(User.name).where(User.email == email)
+    ).scalar_one_or_none()
+    return session_user
