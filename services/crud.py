215801c2ab55964aceb51e915b68130b49a63e5a
lincolntaranto <54151306+lincolntaranto@users.noreply.github.com>
2026-06-30 14:06:43 -0300

    docs: documenta a funcao 'get_user_by_email'

--- services/crud.py
+++ services/crud.py
@@ -5,6 +5,16 @@ from models import User
 
 
 def get_user_by_email(*, session: Session, email: str) -> User | None:
+    """Procura o usuário pelo email fornecido.
+
+    Args:
+        session (Session): sessão do banco de dados.
+        email (str): email a ser procurado.
+
+    Returns:
+        User: retorna o usuário caso seja encontrado.
+        None: caso não encontrado retorna None.
+    """
     session_user = session.execute(
         select(User).where(User.email == email)
     ).scalar_one_or_none()
